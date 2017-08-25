c87d344b7e4a23c3524d089741d4faef8307cd34
Nofrets <opetstudio@gmail.com>
2017-08-25 21:55:01 +0700

    last change

--- src/containers/UsersContainer/UsersContainerRender.native.js
+++ src/containers/UsersContainer/UsersContainerRender.native.js
@@ -3,7 +3,8 @@ import {
   StyleSheet,
   View,
   Text,
-  TouchableHighlight
+  TouchableHighlight,
+  SectionList
 } from 'react-native';
 // import PropTypes from 'prop-types';
 // import { fetchData } from '../../../actions/fetchData';
@@ -17,18 +18,29 @@ export default function () {
           <Text style={styles.buttonText}>Load Data</Text>
         </TouchableHighlight>
         <UsersList title="Users">
+          <SectionList
+            sections={[
+              { title: 'D', data: this.props.users.data },
+              {
+                title: 'J', data: this.props.users.data
+              },
+            ]}
+            renderItem={({ item }) => <Text style={styles.item}>{item.name} ({item.age})</Text>}
+            renderSectionHeader={
+              ({ section }) => <Text style={styles.sectionHeader}>{section.title}</Text>}
+          />
           {
-            this.props.users.data.length ?
-            (
-              this.props.users.data.map(
-                (person, i) => (
-                  <View key={i} >
-                    <Text>Name: {person.name}</Text>
-                    <Text>Age: {person.age}</Text>
-                  </View>
-                )
-              )
-            ) : null
+            // this.props.users.data.length ?
+            // (
+            //   this.props.users.data.map(
+            //     (person, i) => (
+            //       <View key={i} >
+            //         <Text>Name: {person.name}</Text>
+            //         <Text>Age: {person.age}</Text>
+            //       </View>
+            //     )
+            //   )
+            // ) : null
           }
         </UsersList>
       </View>
@@ -41,6 +53,15 @@ const styles = StyleSheet.create({
   },
   text: {
     textAlign: 'center'
+  },
+  sectionHeader: {
+    paddingTop: 2,
+    paddingLeft: 10,
+    paddingRight: 10,
+    paddingBottom: 2,
+    fontSize: 14,
+    fontWeight: 'bold',
+    backgroundColor: 'rgba(247,247,247,1.0)'
   },
   button: {
     height: 60,
