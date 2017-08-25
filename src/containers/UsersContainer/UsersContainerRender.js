4f99b94f06e1b410bd025480e9b045e1c1ef58cf
Nofrets <opetstudio@gmail.com>
2017-08-25 09:49:43 +0700

    using Table, Lable and Button from semantic ui

--- src/containers/UsersContainer/UsersContainerRender.js
+++ src/containers/UsersContainer/UsersContainerRender.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Button, Table, Label } from 'semantic-ui-react';
 // import PropTypes from 'prop-types';
 // import { fetchData } from '../../../actions/fetchData';
 import UsersList from '../../components/UsersList/UsersList';
@@ -7,23 +8,37 @@ export default function () {
     return (
       <div>
         <span>Members Page Desktop/Web</span>
-        <button onClick={() => { this._onPressButton(); }}>
+        <Button onClick={() => { this._onPressButton(); }}>
           Load Data
-        </button>
+        </Button>
         <UsersList title="Users">
-          {
-            this.props.users.data.length ?
-            (
-              this.props.users.data.map(
-                (person, i) => (
-                    <div key={i} >
-                      <span>Name: {person.name}</span>
-                      <span>Age: {person.age}</span>
-                    </div>
-                )
-              )
-            ) : null
-          }
+          <Table celled>
+            <Table.Header>
+              <Table.Row>
+                <Table.HeaderCell>Name</Table.HeaderCell>
+                <Table.HeaderCell>Age</Table.HeaderCell>
+              </Table.Row>
+            </Table.Header>
+            <Table.Body>
+              {
+                this.props.users.data.length ?
+                (
+                  this.props.users.data.map(
+                    (person, i) => (
+                        <Table.Row key={i}>
+                          <Table.Cell>
+                            <Label ribbon>{person.name}</Label>
+                          </Table.Cell>
+                          <Table.Cell>
+                            <Label ribbon>{person.age}</Label>
+                          </Table.Cell>
+                        </Table.Row>
+                    )
+                  )
+                ) : null
+              }
+            </Table.Body>
+          </Table>
         </UsersList>
       </div>
   );
